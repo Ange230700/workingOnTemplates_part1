3af6c8b079b46d6e0952d751e8901131b9f7b221
Ange230700 <angeericstephane.kouakou@ynov.com>
2023-07-27 21:59:57 +0200

    keeping editing the main JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,10 @@
         $window.on('resize.flexbox-fix', function () {
             clearTimeout(flexboxFixTimeoutId);
             flexboxFixTimeoutId = setTimeout(function () {
-
+                if ($wrapper.prop('scrollHeight') > $window.height())
+                    $wrapper.css('height', 'auto');
+                else
+                    $wrapper.css('height', '100vh')
             }, 250)
         }).triggerHandler('resize.flexbox-fix');
     }
@@ -52,7 +55,7 @@
         locked = false;
 
     // Methods.
-    $main._show = function(id, initial){
+    $main._show = function (id, initial) {
         var $article = $main_articles.filter('#' + id);
 
         // No such article? Bail.
@@ -62,7 +65,7 @@
         // Handle lock.
 
         // Already locked? Speed through "show" steps w/o delays.
-        if(locked || (typeof initial != 'undefined' && initial === true)){
+        if (locked || (typeof initial != 'undefined' && initial === true)) {
             $body.addClass('is-article-visible');
             $body.addClass('is-switching');
             $main_articles.removeClass('active');
@@ -72,7 +75,7 @@
             $article.show();
             $article.addClass('active');
             locked = false;
-            setTimeout(function(){
+            setTimeout(function () {
                 $body.removeClass('is-switching');
             }, (initial ? 1000 : 0));
             return;
@@ -80,16 +83,101 @@
 
         locked = true;
 
-        if($body.hasClass('is-article-visible')){
+        if ($body.hasClass('is-article-visible')) {
             var $currentArticle = $main_articles.filter('.active');
             $currentArticle.removeClass('active');
-            setTimeout(function(){
+            setTimeout(function () {
                 $currentArticle.hide();
                 $article.show();
-                setTimeout(function(){
-
+                setTimeout(function () {
+                    $article.addClass('active');
+                    $window
+                        .scrollTop(0)
+                        .triggerHandler('resize.flexbox-fix');
                 }, 25);
             }, delay);
         }
-    }
+
+        else {
+            $body
+                .addClass('is-article-visible');
+            setTimeout(function () {
+                $header.hide();
+                $footer.hide();
+                $main.show();
+                $article.show();
+                setTimeout(function () {
+                    $article.addClass('active');
+                    $window
+                        .scrollTop(0)
+                        .triggerHandler('resize.flexbox-fix');
+                    setTimeout(function () {
+                        locked = false;
+                    }, delay)
+                }, 25);
+            }, delay)
+        }
+    };
+
+    $main._hide = function (addState) {
+        var $article = $main_articles.filter('.active');
+        if (!$body.hasClass('is-article-visible'))
+            return;
+        if (typeof addState != 'undefined' && addState === true)
+            history.pushState(null, null, '#')
+
+        if (locked) {
+            $body.addClass('is-switching');
+            $article.removeClass('active');
+            $article.hide();
+            $main.hide();
+            $header.show();
+            $footer.show();
+            $body.removeClass('is-article-visible');
+            locked = false;
+            $body.removeClass('is-switching');
+            $window
+                .scrollTop(0)
+                .triggerHandler('resize.flexbox-fix');
+            return;
+        }
+
+        locked = true;
+        $article.removeClass('active');
+        setTimeout(function () {
+            $article.hide();
+            $main.hide();
+            $header.show();
+            $footer.show();
+            setTimeout(function () {
+                $body.removeClass('is-article-visible');
+                $window
+                    .scrollTop(0)
+                    .triggerHandler('resize.flexbox-fix');
+                setTimeout(function () {
+                    locked = false;
+                }, delay)
+            }, 25);
+        }, delay);
+    };
+
+    $main_articles.each(function () {
+        var $this = $(this);
+        $('<div class="close">Close</div>')
+            .appendTo($this)
+            .on('click', function () {
+                location.hash = '';
+            });
+
+        $this.on('click', function (event) {
+            event.stopPropagation();
+        });
+    });
+
+    $body.on('click', function (event) {
+        if ( $body.hasClass('is-article-visible') )
+            $main._hide(true);
+    });
+
+    
 })(jQuery);
